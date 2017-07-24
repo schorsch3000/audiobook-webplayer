4ead00e5acc50dfd4b1bf0ceede5cd43def2cf83
Dirk Heilig <dicky.dicky@gmail.com>
2017-07-24 21:29:12 +0200

    bundle frontend into single html

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
 module.exports = {
-    entry: './src/frontend.js',
-    output: {
-        filename: 'frontend.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: "Audiobook Player"
-    })],
+  entry: './src/frontend.js',
+  output: {
+    filename: 'frontend.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Audiobook Player',
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      inline: 'frontend.js',
+    }),
+  ],
 
-    module: {
-        loaders: [
-            {include: /\.pug$/, loader: ['pug-loader']},
-            {test: /\.css$/, loader: "style-loader!css-loader"},
-            {test: /\.sass$/, loader: "style-loader!css-loader!sass-loader"},
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
-                    }
-                }
-            }
-        ]
-    }
+  module: {
+    loaders: [
+      { include: /\.pug$/, loader: ['pug-loader'] },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.sass$/, loader: 'style-loader!css-loader!sass-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
 };
